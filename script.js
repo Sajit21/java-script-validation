b8c250e3efe67fb12af14534a587b65e413af623
Sajit21 <126838978+Sajit21@users.noreply.github.com>
2023-11-01 21:20:10 +0545

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,6 @@
-//alert('prajwol is ');
-function validation()
-{
-   // alert('prajwol i');
-  // document.getElementById("trigger").addEventListener("click",validation);
-var name = document.getElementById('name').value;
-var address = document.getElementById('address').value;
-if (name == ''){
-    //alert('name is required');
-    document.getElementById('nameErr').innerHTML="name is required";
-    document.getElementById('name').focus();
-}
-if(address == ''){
-   // alert('address is required');
-   document.getElementById('addressErr').innerHTML="Address is required";
-
-}
-}
-document.getElementById("myform").addEventListener("submit",validation);
+onload = () => {
+    const c = setTimeout(() => {
+      document.body.classList.remove("not-loaded");
+      clearTimeout(c);
+    }, 1000);
+  };
